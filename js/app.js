86b32b82a04a81c83cee6105777a0cfc29009d7e
kiddy-11 <98145022+kiddy-11@users.noreply.github.com>
2022-05-27 20:04:22 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$ (function () {
+    $('.slider_imge').slick ({
+        prevArrow:'<i class="image_icon1 fas fa-long-arrow-left">',
+        nextArrow:'<i class="image_icon2 fas fa-long-arrow-right">',
+     
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed:500,
+    })
+    
+})
+
+
+
